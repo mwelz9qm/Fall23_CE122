bf2316cfa8d7a5e1d8d83e9a5903bde81fd7c4cf
Matt Welz <matthew.d.welz@gmail.com>
2023-09-13 00:10:51 -0600

    added stuff

--- in_class/ch3_in_out.cpp
+++ in_class/ch3_in_out.cpp
@@ -17,19 +17,29 @@ int main()
     state1 = "Colorado";
     state2 = "Illinois";
     
-    ofstream outfile;
-    outfile.open("prettytable.txt");
+    ofstream outFile;
+    outFile.open("prettytable.txt");
 
+    ifstream inFile;
+    inFile.open("stuff.txt");
+    double a,b,c;
 
     cout << left;
     cout << setw(10) << "name " << setw(10) << "city " << setw(10) << "state " << endl;
     cout << setw(10) << name1 << setw(10) << city1 << setw(10) << state1 << endl;
     cout << setw(10) << name2 << setw(10) << city2 << setw(10) << state2 << endl;
 
-    outfile << left;
-    outfile << setw(10) << "name " << setw(10) << "city " << setw(10) << "state " << endl;
-    outfile << setw(10) << name1 << setw(10) << city1 << setw(10) << state1 << endl;
-    outfile << setw(10) << name2 << setw(10) << city2 << setw(10) << state2 << endl;
+    outFile << left;
+    outFile << setw(10) << "name " << setw(10) << "city " << setw(10) << "state " << endl;
+    outFile << setw(10) << name1 << setw(10) << city1 << setw(10) << state1 << endl;
+    outFile << setw(10) << name2 << setw(10) << city2 << setw(10) << state2 << endl;
+
+    inFile >> a >> b >> c;
+    
+    outFile << setprecision(2) << fixed << right;
+    outFile << setw(8) << a << setw(8) << b << setw(8) << c << endl;
+    outFile << setw(8) << 2*a << setw(8) << 2*b << setw(8) << 2*c << endl;
+    outFile << setw(8) << 3*a << setw(8) << 3*b << setw(8) << 3*c << endl;
 
     return 0;
 }
