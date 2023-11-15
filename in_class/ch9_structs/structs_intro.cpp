ea519e758892746a8088f2c84a567a94adec8ab8
Matt Welz <matthew.d.welz@gmail.com>
2023-11-14 20:43:26 -0700

    prelim struct example

---
+++ in_class/ch9_structs/structs_intro.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{   enum wallStyle{DRYWALL, PLASTER};
+
+    //hopetype allows us to have records containing a variety of datatypes to represent a house
+    struct homeType
+    {
+        string style;
+        int numBedrooms;
+        int numBaths;
+        int yearBuilt;
+        wallStyle wallType;
+        double price;
+        double tax;
+    };
+
+    homeType myHouse;
+
+    //setting the member variables for myHouse
+    myHouse.style = "Cape Cod";
+    myHouse.numBedrooms = 3;
+    myHouse.numBaths = 2;
+    myHouse.yearBuilt = 1950;
+    myHouse.wallType = DRYWALL;
+    myHouse.price = 456789.10;
+    myHouse.tax = myHouse.price * .01;  //set house tax equal to 1% of home price
+
+    //printing out some of the contents of my house
+
+    cout << "Home style: " << myHouse.style << endl;
+    cout << "Year built: " << myHouse.yearBuilt << endl;
+    cout << "Purchase price: " << myHouse.price << endl;
+
+    return 0;
+}
